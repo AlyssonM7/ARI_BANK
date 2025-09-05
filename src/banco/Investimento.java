8a7b48aee2239684655c2eecd82cdcd0e202180d
ricardolucena62-dev <ricardolucena62@gmail.com>
2025-09-05 15:47:27 -0300

    Caros, comecei a implementar a class Investimentos.

--- src/banco/Investimento.java
+++ src/banco/Investimento.java
@@ -2,4 +2,21 @@ package banco;
 
 import java.util.Scanner;
 
-public class 
+public class Investimento{
+    double Carteira;
+
+
+    public double aplicacaoCDB12Meses(Carteira){
+        
+        
+        if(Banco.saldo  == 0){
+            System.out.println("Saldo Insuficiente.")
+        }
+
+    }
+
+
+
+
+
+}
